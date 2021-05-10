ffe12e8094d1f608b98a57d109ec3f9f652521a6
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-05-11 00:34:06 +0900

    change components to file

--- App.js
+++ App.js
@@ -1,53 +1,62 @@
 import './App.css';
 //App 이라는 컴포넌트의 디자인을 그 앱 안에 넣는다는 의미
-import { Component } from 'react';
+import React ,{ Component } from 'react';
+//React 사용시 필수 import
+import Link from "./components/Link";
+//components 의 Link에서 가져온다
+import ARTICLE from "./components/ARTICLE";
+import Subject from "./components/Subject";
 
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        {/* component 는 반드시 하나의 최상위 태그만 써야함 (ex. header) */}
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-        {/* 다음과 같이 수정한다면 --> 지정해놓은 title 을 위치시키겠다
-        --> 지정해놓은 sub 를 다음과 같이 위치시키겠다 라는 의미가 된다 */}
-      </header>
-    );
-  }
-}
 
-class LITAG extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
+//src 폴더에 components 폴더 생성 후 , 사용할 Component 들을 파일화 해서 App.js 파일에 삽입
 
-class ARTICLE extends Component{
-  render(){
-    return(
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
+// class Subject extends Component{
+//   render(){
+//     return(
+//       <header>
+//         {/* component 는 반드시 하나의 최상위 태그만 써야함 (ex. header) */}
+//         <h1>{this.props.title}</h1>
+//         {this.props.sub}
+//         {/* 다음과 같이 수정한다면 --> 지정해놓은 title 을 다음과 같이 위치시키겠다
+//         --> 지정해놓은 sub 를 다음과 같이 위치시키겠다 라는 의미가 된다 */}
+//       </header>
+//     );
+//   }
+// }
 
-    );
-  }
-}
+// class Link extends Component{
+//   render()
+//   {
+//       return(
+//           <nav>
+//               <ul>
+//                   <li><a href="1.html">HTML</a></li>
+//                   <li><a href="2.html">CSS</a></li>
+//                   <li><a href="3.html">JavaScript</a></li>
+//               </ul>
+//           </nav>
+//       );
+//   }
+// }
+
+// class ARTICLE extends Component{
+//   render(){
+//     return(
+//       <article>
+//         <h2>{this.props.title}</h2>
+//         {this.props.desc}
+//       </article>
+
+//     );
+//   }
+// }
 
 class App extends Component{
   render(){
     return(
       <div classname="App">
         <Subject title="WEB" sub="REACT"></Subject>
-        <LITAG></LITAG>
+        <Link></Link>
         <ARTICLE title="HTML" desc="HTML is HyperText Markup Language"></ARTICLE>
       </div>
     ); 
