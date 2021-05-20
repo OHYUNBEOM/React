fa3a3195e259ac3319ff3dfd088e9444003db05c
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-05-21 00:19:28 +0900

    made event

--- components_js/Subject.js
+++ components_js/Subject.js
@@ -4,7 +4,11 @@ class Subject extends Component{
         return(
             <header>
             {/* component 는 반드시 하나의 최상위 태그만 써야함 (ex. header) */}
-            <h1><a href="/">{this.props.title}</a></h1>
+            <h1><a href="/" onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage();
+                //Subject 라는 Component 에서 onChangePage 이벤트가 발생하게되면 여기서 호출한다.
+            }.bind(this)}>{this.props.title}</a></h1>
             {this.props.sub}
             {/* 다음과 같이 수정한다면 --> 지정해놓은 title 을 다음과 같이 위치시키겠다
           --> 지정해놓은 sub 를 다음과 같이 위치시키겠다 라는 의미가 된다 */}
