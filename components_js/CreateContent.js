d34d6a389081f1aab0f51cb37da82bd28e78da74
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-05-24 00:13:52 +0900

    Create component

---
+++ components_js/CreateContent.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+class CreateContent extends Component{
+    render(){
+        return(
+            <article>
+                <h2>Create</h2>
+                <form>
+
+                </form>
+            </article>
+            );
+        }
+    }
+// create 링크 클릭시에 content 를 아에 createcontent 로 바꾸기위함
+    export default CreateContent;
